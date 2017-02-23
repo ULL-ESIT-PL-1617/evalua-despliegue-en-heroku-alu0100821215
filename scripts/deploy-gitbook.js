d7614762473d410ae898cc5880290954a1e54137
alu0100821215 <alu0100821215@ull.edu.es>
2017-02-23 09:24:53 +0000

    Página web

--- scripts/deploy-gitbook.js
+++
@@ -1,4 +0,0 @@
-var ghpages = require('gh-pages');
-var path = require('primeros-pasos-en-nodejs-victor-jesus-pamela');
- 
-ghpages.publish(path.join(__dirname, 'primeros-pasos-en-nodejs-victor-jesus-pamela'), function(err) {  });
