f19880090c821a6b141c8892f8ed27c99509831e
alu0100821215 <alu0100821215@ull.edu.es>
2017-02-23 09:14:53 +0000

    Cambiado el port

--- servidor.js
+++ servidor.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
+app.ser('port', (process.env.PORT || 5000))
+
 //view engine setup
 app.set('views', path.join(__dirname));
 app.set('view engine', 'ejs');
