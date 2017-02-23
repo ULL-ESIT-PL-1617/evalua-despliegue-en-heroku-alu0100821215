bf46ece3333d69b4c36ff8ad0a41b1aa331bee49
alu0100821215 <alu0100821215@ull.edu.es>
2017-02-23 09:19:24 +0000

    Pequeños arreglos 2

--- servidor.js
+++ servidor.js
@@ -1,11 +1,10 @@
 var express = require("express");
 var app = express();
-var path = require("path");
 
 app.ser('port', (process.env.PORT || 5000));
 
 //view engine setup
-app.set('views', path.join(__dirname));
+app.set('views', __dirname);
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname));
